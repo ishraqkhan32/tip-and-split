0a14e993e4553ac37c255e1ceb5c751fae63aa4e
Ishraq Khan <ishraqkhan@HON-2016226.home>
2017-12-31 00:58:24 -0500

    Initial commit

--- Gratuity/ViewController.swift
+++ Gratuity/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var splitField: UITextField!
+    @IBOutlet weak var eachLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        
+        let tipPercentages = [0.12, 0.15, 0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip: Double = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total: Double = bill + tip
+        let perPerson: Double = total / (Double(splitField.text!) ?? 1)!
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        eachLabel.text = String(format: "$%.2f", perPerson)
+    }
+    
 }
 
